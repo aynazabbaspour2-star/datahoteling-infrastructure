462762ef24b2befac8fc8d457b8a4794609d885f
Aynaz Abbaspour <280726339+aynazabbaspour2-star@users.noreply.github.com>
2026-09-10 10:45:57 +0330

    refactor(terraform): manage production network configuration

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -1,24 +1,32 @@
 data "vsphere_datacenter" "dc" {
-  name = "ha-datacenter"
+  name = var.datacenter_name
 }
 
 data "vsphere_host" "host" {
-  name          = "192.168.1.21"
+  name          = var.esxi_host
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_network" "vm_network" {
-  name          = "VM Network"
+  name          = var.datastore_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_resource_pool" "root" {
   name = "Resources"
+}
+
+module "network" {
+  source = "../../modules/network"
+
+  host_system_id      = data.vsphere_host.host.id
+  virtual_switch_name = var.virtual_switch_name
+  network_name        = var.network_name
+  network_vlan_id     = var.network_vlan_id
+
+  uplink_nics  = var.uplink_nics
+  active_nics  = var.active_nics
+  standby_nics = var.standby_nics
 }
 
 output "datacenter_name" {
@@ -36,12 +44,17 @@ output "datastore_name" {
   value       = data.vsphere_datastore.datastore.name
 }
 
-output "network_name" {
-  description = "Production VM network"
-  value       = data.vsphere_network.vm_network.name
-}
-
 output "resource_pool_id" {
   description = "Production root resource pool ID"
   value       = data.vsphere_resource_pool.root.id
+}
+
+output "managed_virtual_switch" {
+  description = "Terraform-managed production virtual switch"
+  value       = module.network.virtual_switch_name
+}
+
+output "managed_network_name" {
+  description = "Terraform-managed production port group"
+  value       = module.network.port_group_name
 }
